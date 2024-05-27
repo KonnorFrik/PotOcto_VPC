0e8c9cc77692de70aff47715732cab3db70625ef
Konnor <konnor.frik666@gmail.com>
2024-05-27 20:10:53 +0500

    comment out useless test

--- src/tests/mem_dump_test.c
+++ src/tests/mem_dump_test.c
@@ -1,16 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>
+// #include <stdio.h>
+// #include <stdlib.h>
 
-#include "../common/funcs.h"
-#include "../common/funcs.c"
+// #include "../common/funcs.h"
+// #include "../common/funcs.c"
 
-int main() {
-    size_t size = 256;
-    byte arr[size];
-
-    for (size_t i = 0; i < size; ++i) {
-        arr[i] = i;
-    }
-
-    mem_dump(stdout, arr, size);
-}
+// int main() {
+//     size_t size = 256;
+//     byte arr[size];
+//
+//     for (size_t i = 0; i < size; ++i) {
+//         arr[i] = i;
+//     }
+//
+//     mem_dump(stdout, arr, size);
+// }
