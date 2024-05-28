04b7fe74a9e91573ee5200284077d4c42f6179be
Konnor <konnor.frik666@gmail.com>
2024-05-28 22:41:08 +0500

    move function 'dump_memory' to hardware dir

--- src/common/funcs.h
+++ src/common/funcs.h
@@ -1,9 +1,6 @@
 #ifndef __COMMON_FUNCS_H__
 #define __COMMON_FUNCS_H__
 
-#include <stdio.h>
-
-void mem_dump(FILE* fd, void* mem, size_t size);
 void show_error(int code);
 
 #endif
--- src/hardware/memory.h
+++ src/hardware/memory.h
@@ -1,6 +1,10 @@
 #ifndef __MEM_H__
 #define __MEM_H__
 
+#include <stdio.h>
+
 typedef unsigned char MEMORY;
+
+void mem_dump(FILE* fd, void* mem, size_t size);
 
 #endif
