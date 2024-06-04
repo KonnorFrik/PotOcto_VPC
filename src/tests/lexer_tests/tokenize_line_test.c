64919d1bc795c5d77bb61a36a54975dfd648ebf9
Konnor <konnor.frik666@gmail.com>
2024-06-04 19:55:23 +0500

    add test for unknown token

--- src/tests/lexer_tests/tokenize_line_test.c
+++ src/tests/lexer_tests/tokenize_line_test.c
@@ -36,11 +36,65 @@ START_TEST(test_1_token_line) {
 }
 END_TEST
 
+START_TEST(test_2_token_line) {
+    char* line = ";mov r1 2";
+
+    AST* obj = lexer_tokenize_line(line);
+
+    ck_assert_ptr_nonnull(obj);
+    ck_assert_uint_eq(obj->token->type, COMMENT);
+    ck_assert_str_eq(obj->token->line, ";mov");
+    ck_assert_uint_eq(obj->token->value, 0);
+    obj = obj->next;
+
+    ck_assert_ptr_nonnull(obj);
+    ck_assert_uint_eq(obj->token->type, AO_REGISTER);
+    ck_assert_str_eq(obj->token->line, "r1");
+    ck_assert_uint_eq(obj->token->value, 1);
+    obj = obj->next;
+
+    ck_assert_ptr_nonnull(obj);
+    ck_assert_uint_eq(obj->token->type, NUMBER);
+    ck_assert_str_eq(obj->token->line, "2");
+    ck_assert_uint_eq(obj->token->value, 2);
+    obj = obj->next;
+
+    ck_assert_ptr_null(obj);
+
+    ast_destroy(obj);
+}
+END_TEST
+
+START_TEST(test_3_token_line) {
+    char* line = "jmp lbl";
+
+    AST* obj = lexer_tokenize_line(line);
+
+    ck_assert_ptr_nonnull(obj);
+    ck_assert_uint_eq(obj->token->type, KEYWORD);
+    ck_assert_str_eq(obj->token->line, "jmp");
+    ck_assert_uint_eq(obj->token->value, 0);
+    obj = obj->next;
+
+    ck_assert_ptr_nonnull(obj);
+    ck_assert_uint_eq(obj->token->type, UNKNOWN);
+    ck_assert_str_eq(obj->token->line, "lbl");
+    ck_assert_uint_eq(obj->token->value, 0);
+    obj = obj->next;
+
+    ck_assert_ptr_null(obj);
+
+    ast_destroy(obj);
+}
+END_TEST
+
 Suite* lexer_tokenize_line_test() {
     Suite* s = suite_create(NAME);
     TCase* t = tcase_create(TCASE_NAME);
 
     tcase_add_test(t, test_1_token_line);
+    tcase_add_test(t, test_2_token_line);
+    tcase_add_test(t, test_3_token_line);
 
     suite_add_tcase(s, t);
     return s;
